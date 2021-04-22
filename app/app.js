d63ff5cd3ce10780d9297f7405f40cb52bf1f6dc
Matt Halvorson <halvorson34.mh@gmail.com>
2021-04-21 21:55:01 -0500

    create function for start game message to appear in index.html, ran tsc, installed typescript globally

--- app/app.js
+++ app/app.js
@@ -3,5 +3,4 @@ function startGame() {
     var messagesElement = document.getElementById('messages');
     messagesElement.innerText = 'Welcome to MultiMatch! Starting new game...';
 }
-
 document.getElementById('startGame').addEventListener('click', startGame);
